2a9433b895562e75d1eafcca2905b0e023ebdb30
anmol <singjila5@gmail.com>
2026-03-31 12:13:18 +0530

    ijuhd

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+
+const port = 5000; 
+
+const mogourl = "mongodb+srv://anmol:2AgmSHvnyx59HKNf@backend.rfkwu1k.mongodb.net/?appName=backend"
+
+mongoose.connect(mogourl)
+
+.then(()=> console.log(` mongo db connect`))
+.catch(()=>console.log(` mongo db error${e}`))
+
+app.listen(port,() => {console.log(`Server is running on port ${port}  `)})
